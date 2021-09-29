16e4a15c8e910fa8b1621889cad6519a5a92443a
panchihpeng <panzhipeng1007@gmail.com>
2021-09-29 16:45:33 +0800

    feat: response headers

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -17,15 +17,37 @@ const normalizeHeaderName = (headers: any, normalizedName: string): any => {
 const processHeaders = (headers: any, data: any): any => {
   normalizeHeaderName(headers, 'Content-Type')
 
-  if(isPlainObject(data)) {
+  if (isPlainObject(data)) {
     if (headers && !headers['Content-Type']) {
       headers['Content-Type'] = 'application/json;charset=utf-8'
     }
-   }
-   return headers
+  }
+  return headers
 }
 
+const parseHeaders = (headers: string): any => {
 
-export  {
-  processHeaders
+  const parsed = Object.create(null)
+
+  if (!headers) return parsed
+
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+
+    key = key.trim().toLowerCase()
+
+    if (!key) return
+
+    if (val) {
+      val = val.trim()
+    }
+
+    parsed[key] = val
+  })
+  return parsed
+}
+
+export {
+  processHeaders,
+  parseHeaders
 }
