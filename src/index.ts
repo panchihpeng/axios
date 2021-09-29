81171f27b4dcff7abf919ae177f7fb2b53324699
panchihpeng <panzhipeng1007@gmail.com>
2021-09-29 16:04:07 +0800

    feat: get response

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest} from'./helpers/data'
-import {processHeaders} from './helpers/headers'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
@@ -10,24 +10,24 @@ function transformUrl(config: AxiosRequestConfig): string {
 }
 
 function transformHeaders(config: AxiosRequestConfig) {
-  const {headers = {}, data} = config
+  const { headers = {}, data } = config
   return processHeaders(headers, data)
 }
 
-function transformRequestData (config: AxiosRequestConfig):any {
+function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
   config.headers = transformHeaders(config)
-  config.data= transformRequestData(config)
+  config.data = transformRequestData(config)
 
 }
 
-function axios(config: AxiosRequestConfig):void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
 export default axios
