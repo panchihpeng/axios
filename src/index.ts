413d0f03e40822e84faa8217f81bd8577f76b4f2
panchihpeng <panzhipeng1007@gmail.com>
2021-09-29 15:28:44 +0800

    feat: headers

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest} from'./helpers/data'
+import {processHeaders} from './helpers/headers'
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const {headers = {}, data} = config
+  return processHeaders(headers, data)
 }
 
 function transformRequestData (config: AxiosRequestConfig):any {
@@ -14,7 +20,9 @@ function transformRequestData (config: AxiosRequestConfig):any {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data= transformRequestData(config)
+
 }
 
 function axios(config: AxiosRequestConfig):void {
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,4 +12,5 @@ export interface AxiosRequestConfig {
   method?: Method
   params?: any
   data?: any
+  headers?: any
 }
